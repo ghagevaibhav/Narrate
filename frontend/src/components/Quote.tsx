6cc362219768fc301dfd2e422fae99e5fe2b87c3
ghagevaibhav <ghagevaibhav@gmail.com>
2024-12-13 23:01:56 +0530

    frontend: init

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,16 @@
+
+
+export const Quote = () => {
+  return (
+    <div className='bg-gray-100 h-screen flex justify-center flex-col'>
+        <div className="flex justify-center">
+            <div className="max-w-lg text-center text-3xl font-bold">
+                "Life isn’t about waiting for the storm to pass; it’s about learning to dance in the rain."
+            </div>
+        </div>
+        <div className="max-w-md text-center font-bold text-gray-600 text-xl">
+            - Mark Twain
+        </div>
+    </div>
+  )
+}
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,17 @@
+
+import { Quote } from '../components/Quote'
+
+export const Signup = () => {
+  return (
+    <>
+    <div className='grid grid-cols-2'>
+        <div>
+            <div>Signup</div>
+        </div>
+        <div>
+            <Quote />
+        </div>
+    </div>
+    </>
+  )
+}
