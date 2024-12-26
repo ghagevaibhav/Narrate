23e38a6451f2a07f446c6119edf7e5e83cf9c93d
ghagevaibhav <ghagevaibhav@gmail.com>
2024-12-26 15:43:06 +0530

    [added]: signin and signup auth pages

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,69 @@
+import { SignUpSchema } from "@ghagevaibhav/medium-common";
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+
+    const [postInputs, setPostInputs] = useState<SignUpSchema>({
+        username: "",
+        password: "",
+        email: "",
+    })
+
+    return (
+        <div className="h-screen flex flex-col justify-center">
+            <div className="flex justify-center">
+                <div>
+                    <div className="px-10 flex flex-col justify-center">
+                        <div className="text-3xl font-extrabold">
+                            {type === "signup" ? "Create an account" : "Sign into account"}
+                        </div>
+                        <div className="text-slate-400 text-center">
+                            {type === "signin" ? "Don't have an account?" : "Already have an account"}
+                            <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>{type === "signin" ? "Sign up" : "Sign in"}</Link>
+                        </div>
+                    </div>
+                    <div className="pt-4">
+                        <LabelledInput
+                            type="text"
+                            label="Username"
+                            placeholder="testuser"
+                            onChange={(e) => setPostInputs((c) => ({ ...c, username: e.target.value }))}
+                        />
+
+                        <LabelledInput
+                            type="password"
+                            label="Password"
+                            placeholder="********"
+                            onChange={(e) => setPostInputs((c) => ({ ...c, password: e.target.value }))}
+                        />
+
+                        <LabelledInput
+                            type="email"
+                            label="Email"
+                            placeholder="testuser@gmail.com"
+                            onChange={(e) => setPostInputs((c) => ({ ...c, email: e.target.value }))}
+                        />
+                    </div>
+                    <button type="button" className=" w-full mt-8 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signin" ? "Sign in" : "Sign up"}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+interface LabelledInputTypes {
+    label: string;
+    placeholder: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+}
+
+function LabelledInput({ label, placeholder, onChange, type }: LabelledInputTypes) {
+    return (
+        <div className="mt-2">
+            <label className="block mb-2 text-sm font-bold pt-2 text-gray-900 dark:text-black">{label}</label>
+            <input onChange={onChange} type={type} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        </div>
+    )
+}  
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,17 +1,18 @@
 
+import { Auth } from '../components/Auth'
 import { Quote } from '../components/Quote'
 
 export const Signup = () => {
   return (
     <>
-    <div className='grid grid-cols-2'>
+      <div className='grid grid-cols-1 lg:grid-cols-2'>
         <div>
-            <div>Signup</div>
+          <Auth type='signup'/> 
         </div>
-        <div>
-            <Quote />
+        <div className='hidden lg:block'>
+          <Quote />
         </div>
-    </div>
+      </div>
     </>
   )
 }
