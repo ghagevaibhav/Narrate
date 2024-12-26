a723c56c2174714c23205db812538ef429da8ead
ghagevaibhav <ghagevaibhav@gmail.com>
2024-12-26 19:30:14 +0530

    zipped npm package

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,7 +3,7 @@ import z from 'zod'
 export const signUpSchema = z.object({
     email: z.string().email(),
     password: z.string().min(6),
-    name: z.string().optional()
+    username: z.string().optional()
 })
 
 
