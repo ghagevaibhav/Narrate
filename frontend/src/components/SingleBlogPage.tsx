d8247ccfa6cc5b7c158e30798b8d00c5dba51ff4
ghagevaibhav <ghagevaibhav@gmail.com>
2024-12-27 17:27:58 +0530

    Added publish component and added the routing logic

--- frontend/src/components/SingleBlogPage.tsx
+++ frontend/src/components/SingleBlogPage.tsx
@@ -8,7 +8,7 @@ export const SingleBlogPage = ({ blog }: {blog: Blog}) => {
         <AppBar />
         <div className="flex justify-center">
             <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-                <div className="col-span-8 m-2">
+                <div className="col-span-8 m-2 mr-4">
                     <div className="text-5xl font-extrabold">
                         {blog.title}
                     </div>
@@ -20,7 +20,7 @@ export const SingleBlogPage = ({ blog }: {blog: Blog}) => {
                     </div>
                 </div>
                 <div className="col-span-4 m-2">
-                    <div className="text-slate-600 text-lg">
+                    <div className="text-slate-600 text-lg mb-4">
                         Author
                     </div>
                     <div className="flex w-full">
